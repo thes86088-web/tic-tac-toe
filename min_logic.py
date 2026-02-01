ca48702f9e4c751e480df38ab87403eaa001924f
durvesh <thes86088@gmail.com>
2026-02-01 19:26:09 +0530

    add bodies for required functions

--- min_logic.py
+++ min_logic.py
@@ -1,14 +1,63 @@
 
-def profitableToPlayer( canvas, i, j) :
+def profitableToPlayer( canvas, i, j, n=3 ) :
+  left = j-1>-1 and (canvas[i][j-1] == 1)
+  right = j+1<n and (canvas[i][j+1] == 1)
 
-def profitableToPlayer( canvas, i, j) :
+  
+  up = i-1>-1 and (canvas[i-1][j] == 1)
+  down = i+1<n and (canvas[i+1][j] == 1)
 
-def placeTheMove( canvas, row, column, n=3 ) :
+  left_up = j-1>-1 and i-1>-1 and (canvas[i-1][j-1] == 1)
+  right_up = j+1<n and i-1>-1 and (canvas[i-1][j+1] == 1)
+
+  
+  left_down = i+1<n and j-1>-1 and (canvas[i+1][j-1] == 1)
+  right_down = i+1<n and j+1<n and (canvas[i+1][j+1] == 1)
+  
+  if( left_up or right_down ) :
+    return 1
+  elif( right_up or left_down ) :
+    return 1
+  elif( (left and right) or ( up and down  ) ) :
+    return 1
+    else :
+      return 1
+    
+  
+
+def profitableToSystem( canvas, i, j, n=3 ) :
+  left = j-1>-1 and (canvas[i][j-1] == -1)
+  right = j+1<n and (canvas[i][j+1] == -1)
+
+  
+  up = i-1>-1 and (canvas[i-1][j] == -1)
+  down = i+1<n and (canvas[i+1][j] == -1)
+
+  left_up = j-1>-1 and i-1>-1 and (canvas[i-1][j-1] == -1)
+  right_up = j+1<n and i-1>-1 and (canvas[i-1][j+1] == -1)
+
+  
+  left_down = i+1<n and j-1>-1 and (canvas[i+1][j-1] == -1)
+  right_down = i+1<n and j+1<n and (canvas[i+1][j+1] == -1)
+  
+  if( left_up or right_down ) :
+    return 1
+  elif( right_up or left_down ) :
+    return 1
+  elif( (left and right) or ( up and down  ) ) :
+    return 1
+    else :
+      return 1
+
+def occupy( canvas, i, j ) :
+  canvas[i][j] = -1
+
+def placeTheMove( canvas, n=3 ) :
   for i in range( n ) :
     for j in range( n ) :
-      if( canvas[i][j} == 0  ) :
+      if( canvas[i][j] == 0  ) :
         if ( profitableToPlayer( canvas, i, j) ) : 
           occupy( canvas, i, j )
-        else if( profitableToSystem( canvas, i, j ) ) :
+        elif( profitableToSystem( canvas, i, j ) ) :
           occupy( canvas, i, j )
 
