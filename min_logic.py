38e520d75fd7a0be59d451d797b1f9f8d2eabc0d
durvesh <thes86088@gmail.com>
2026-02-01 19:31:16 +0530

    consolidate profitable__() methods
    maintain a list of available positions
    
    stop the game loop when this list becomes empty

--- min_logic.py
+++ min_logic.py
@@ -1,18 +1,16 @@
 
-def profitableToPlayer( canvas, i, j, n=3 ) :
-  left = j-1>-1 and (canvas[i][j-1] == 1)
-  right = j+1<n and (canvas[i][j+1] == 1)
-
+def profitableCell( canvas, i, j, value, n=3 ) :
+  left = j-1>-1 and (canvas[i][j-1] == value)
+  right = j+1<n and (canvas[i][j+1] == value)
   
-  up = i-1>-1 and (canvas[i-1][j] == 1)
-  down = i+1<n and (canvas[i+1][j] == 1)
+  up = i-1>-1 and (canvas[i-1][j] == value)
+  down = i+1<n and (canvas[i+1][j] == value)
 
-  left_up = j-1>-1 and i-1>-1 and (canvas[i-1][j-1] == 1)
-  right_up = j+1<n and i-1>-1 and (canvas[i-1][j+1] == 1)
+  left_up = j-1>-1 and i-1>-1 and (canvas[i-1][j-1] == value)
+  right_up = j+1<n and i-1>-1 and (canvas[i-1][j+1] == value)
 
-  
-  left_down = i+1<n and j-1>-1 and (canvas[i+1][j-1] == 1)
-  right_down = i+1<n and j+1<n and (canvas[i+1][j+1] == 1)
+  left_down = i+1<n and j-1>-1 and (canvas[i+1][j-1] == value)
+  right_down = i+1<n and j+1<n and (canvas[i+1][j+1] == value)
   
   if( left_up or right_down ) :
     return 1
@@ -22,32 +20,7 @@ def profitableToPlayer( canvas, i, j, n=3 ) :
     return 1
     else :
       return 1
-    
   
-
-def profitableToSystem( canvas, i, j, n=3 ) :
-  left = j-1>-1 and (canvas[i][j-1] == -1)
-  right = j+1<n and (canvas[i][j+1] == -1)
-
-  
-  up = i-1>-1 and (canvas[i-1][j] == -1)
-  down = i+1<n and (canvas[i+1][j] == -1)
-
-  left_up = j-1>-1 and i-1>-1 and (canvas[i-1][j-1] == -1)
-  right_up = j+1<n and i-1>-1 and (canvas[i-1][j+1] == -1)
-
-  
-  left_down = i+1<n and j-1>-1 and (canvas[i+1][j-1] == -1)
-  right_down = i+1<n and j+1<n and (canvas[i+1][j+1] == -1)
-  
-  if( left_up or right_down ) :
-    return 1
-  elif( right_up or left_down ) :
-    return 1
-  elif( (left and right) or ( up and down  ) ) :
-    return 1
-    else :
-      return 1
 
 def occupy( canvas, i, j ) :
   canvas[i][j] = -1
@@ -56,8 +29,8 @@ def placeTheMove( canvas, n=3 ) :
   for i in range( n ) :
     for j in range( n ) :
       if( canvas[i][j] == 0  ) :
-        if ( profitableToPlayer( canvas, i, j) ) : 
+        if ( profitableCell( canvas, i, j, 1, n=3 ) ) : 
           occupy( canvas, i, j )
-        elif( profitableToSystem( canvas, i, j ) ) :
+        elif( profitableCell( canvas, i, j, -1, n=3 ) ) :
           occupy( canvas, i, j )
 
